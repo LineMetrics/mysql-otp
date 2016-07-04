bbfbb4d908e4933a70d1f0f3b046525427ac286d
LineMetrics <tec-team@linemetrics.com>
2016-07-04 21:22:28 +0200

    timeout 4 sec

--- src/mysql_protocol.erl
+++ src/mysql_protocol.erl
@@ -74,7 +74,7 @@ quit(TcpModule, Socket) ->
 -spec ping(atom(), term()) -> #ok{}.
 ping(TcpModule, Socket) ->
    Query = <<"SELECT 1;">>,
-   query(Query, TcpModule, Socket, 3000).
+   query(Query, TcpModule, Socket, 4000).
 
 -spec query(Query :: iodata(), atom(), term(), timeout()) ->
     {ok, [#ok{} | #resultset{} | #error{}]} | {error, timeout}.
